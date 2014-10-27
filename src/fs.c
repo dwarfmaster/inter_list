863cf854d6eab4aaf6cd6bce5d37c890af203281
lucas8 <luc.linux@mailoo.org>
2014-10-27 10:07:31 +0100

    Implemented the _fs_stat callback.

--- src/fs.c
+++ src/fs.c
@@ -1,12 +1,16 @@
 
 #include "fs.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <ixp.h>
 
 /* The QID of the first line. */
 #define QID_LIST_OFFSET 15
 /* The number of QIDs between two lines. */
 #define QID_LIST_STEP 5
+/* The QID of the line dir field in a line. */
+#define QID_LIST_LINE 0
 /* The QID of the text field in a line. */
 #define QID_LIST_TEXT 1
 /* The QID of the name field in a line. */
@@ -15,6 +19,7 @@
 #define QID_LIST_SHOW 3
 
 /* Macros to get the QID of the field of a line. */
+#define GET_LINE_QID(n) (QID_LIST_OFFSET + QID_LIST_STEP * (n) + QID_LIST_LINE)
 #define GET_TEXT_QID(n) (QID_LIST_OFFSET + QID_LIST_STEP * (n) + QID_LIST_TEXT)
 #define GET_NAME_QID(n) (QID_LIST_OFFSET + QID_LIST_STEP * (n) + QID_LIST_NAME)
 #define GET_SHOW_QID(n) (QID_LIST_OFFSET + QID_LIST_STEP * (n) + QID_LIST_SHOW)
@@ -84,9 +89,112 @@ static void _fs_walk(Ixp9Req* r)
     /* TODO */
 }
 
+static void _fs_dostat(IxpStat* st, uint32_t path)
+{
+    if(path <= QID_END) {
+        st->type     = P9_QTFILE;
+        st->qid.type = P9_QTFILE;
+        st->qid.path = path;
+        st->mode     = 0600;
+        st->length   = 0;
+        switch(path) {
+            case QID_ROOT:
+                st->type     = P9_QTDIR;
+                st->qid.type = P9_QTDIR;
+                st->mode     = 0500 | P9_DMDIR;
+                st->name     = "";
+                break;
+            case QID_CTL:
+                st->mode   = 0200;
+                st->name   = "ctl";
+                break;
+            case QID_FEED:
+                st->name   = "feed";
+                break;
+            case QID_BINDINGS:
+                st->name   = "bindings";
+                break;
+            case QID_TOP:
+                st->name   = "top";
+                break;
+            case QID_BOT:
+                st->name   = "bot";
+                break;
+            case QID_COLORS:
+                st->name   = "colors";
+                st->length = 8; /* The number of color pairs. */
+                break;
+            case QID_LIST:
+                st->type     = P9_QTDIR;
+                st->qid.type = P9_QTDIR;
+                st->mode     = 0500 | P9_DMDIR;
+                st->name     = "list";
+                st->length   = 0; /* TODO get the number of lines. */
+                break;
+            case QID_SCROLL:
+                st->name = "scroll";
+                break;
+            case QID_SELECTION:
+                st->name = "selection";
+                break;
+            default:
+                /* Shouldn't happen. */
+                break;
+        }
+    }
+    else {
+        size_t id = GET_LINE_ID(path);
+        char buffer[64];
+
+        st->type     = P9_QTFILE;
+        st->qid.type = P9_QTFILE;
+        st->qid.path = path;
+        st->mode     = 0400;
+        st->length   = 0;
+        switch(GET_FIELD_ID(path)) {
+            case QID_LIST_LINE:
+                snprintf(buffer, 64, "%lu", id);
+                st->type     = P9_QTDIR;
+                st->qid.type = P9_QTDIR;
+                st->mode     = 0500 | P9_DMDIR;
+                /* TODO may break : buffer not available when leaving. */
+                st->name     = buffer;
+                break;
+            case QID_LIST_TEXT:
+                st->name = "text";
+                break;
+            case QID_LIST_NAME:
+                st->name = "name";
+                break;
+            case QID_LIST_SHOW:
+                st->mode = 0600;
+                st->name = "show";
+                break;
+            default:
+                /* Shouldn't happen. */
+                break;
+        }
+    }
+    st->uid = st->gid = st->muid = "";
+}
+
 static void _fs_stat(Ixp9Req* r)
 {
-    /* TODO */
+    IxpStat st = { 0 };
+    IxpMsg m;
+    char buf[512];
+
+    m = ixp_message(buf, sizeof(buf), MsgPack);
+    _fs_dostat(&st, r->fid->qid.path);
+    ixp_pstat(&m, &st);
+    r->ofcall.rstat.nstat = ixp_sizeof_stat(&st);
+    if(!(r->ofcall.rstat.stat = malloc(r->ofcall.rstat.nstat))) {
+        r->ofcall.rstat.nstat = 0;
+        ixp_respond(r, "out of memory");
+        return;
+    }
+    memcpy(r->ofcall.rstat.stat, m.data, r->ofcall.rstat.nstat);
+    ixp_respond(r, NULL);
 }
 
 static void _fs_read(Ixp9Req* r)
